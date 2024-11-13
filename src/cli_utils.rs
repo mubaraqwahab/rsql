13ec3bd2dcbeb5529e232a315c3b594779f1b515
Mubaraq Wahab <mub.wahab26@gmail.com>
2024-11-13 21:42:22 +0100

    move stuff from main to cli_utils

---
+++ src/cli_utils.rs
@@ -0,0 +1,112 @@
+use dialoguer::theme::Theme;
+use prettytable as pt;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt;
+
+pub struct Connection<'a> {
+    url: &'a str,
+    client: reqwest::Client,
+}
+
+impl<'a> Connection<'a> {
+    pub fn new(url: &'a str) -> Self {
+        Self {
+            url,
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn execute(&self, query: &str) -> Result<QueryResult, Box<dyn std::error::Error>> {
+        let mut map = HashMap::new();
+        map.insert("query", query);
+        map.insert("url", self.url);
+
+        let resp = self
+            .client
+            .post("http://localhost:9876")
+            .json(&map)
+            .send()
+            .await?;
+
+        let query_result: QueryResult = resp.json().await?;
+        Ok(query_result)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct QueryResult {
+    rows: Vec<HashMap<String, serde_json::Value>>,
+    columns: Vec<QueryResultColumn>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct QueryResultColumn {
+    name: String,
+}
+
+pub struct CliTheme;
+
+impl Theme for CliTheme {
+    /// Formats an input prompt.
+    fn format_input_prompt(
+        &self,
+        f: &mut dyn fmt::Write,
+        prompt: &str,
+        default: Option<&str>,
+    ) -> fmt::Result {
+        match default {
+            Some(default) if prompt.is_empty() => write!(f, "[{}] ", default),
+            Some(default) => write!(f, "{} [{}] ", prompt, default),
+            None => write!(f, "{} ", prompt),
+        }
+    }
+
+    /// Formats an input prompt after selection.
+    #[inline]
+    fn format_input_prompt_selection(
+        &self,
+        f: &mut dyn fmt::Write,
+        prompt: &str,
+        sel: &str,
+    ) -> fmt::Result {
+        write!(f, "{} {}", prompt, sel)
+    }
+}
+
+pub async fn run_query(
+    query: &str,
+    conn: &Connection<'_>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let QueryResult { rows, columns } = conn.execute(query).await?;
+
+    if columns.is_empty() {
+        return Ok(());
+    }
+
+    let mut table = pt::Table::new();
+    table.set_format(*pt::format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+    let column_names: Vec<&String> = columns.iter().map(|c| &c.name).collect();
+    table.set_titles(column_names.iter().into());
+
+    for row in &rows {
+        table.add_row(
+            column_names
+                .iter()
+                .map(|&c| match row.get(c).unwrap() {
+                    serde_json::Value::String(v) => v.clone(),
+                    serde_json::Value::Number(v) => v.to_string(),
+                    serde_json::Value::Bool(v) => v.to_string(),
+                    serde_json::Value::Null => String::from(""),
+                    v => panic!("Failed to parse json value {}", v),
+                })
+                .into(),
+        );
+    }
+
+    table.printstd();
+    println!("({} rows)", rows.len());
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,8 @@
-use dialoguer::{theme::Theme, BasicHistory, Input};
-use prettytable as pt;
+mod cli_utils;
+
+use crate::cli_utils::*;
+use dialoguer::{BasicHistory, Input};
 use regex::Regex;
-use serde::Deserialize;
-use std::collections::HashMap;
-use std::fmt;
-
-struct Connection<'a> {
-    url: &'a str,
-    client: reqwest::Client,
-}
-
-impl<'a> Connection<'a> {
-    fn new(url: &'a str) -> Self {
-        Self {
-            url,
-            client: reqwest::Client::new(),
-        }
-    }
-
-    async fn execute(&self, query: &str) -> Result<QueryResult, Box<dyn std::error::Error>> {
-        let mut map = HashMap::new();
-        map.insert("query", query);
-        map.insert("url", self.url);
-
-        let resp = self
-            .client
-            .post("http://localhost:9876")
-            .json(&map)
-            .send()
-            .await?;
-
-        let query_result: QueryResult = resp.json().await?;
-        Ok(query_result)
-    }
-}
-
-#[derive(Debug, Deserialize)]
-struct QueryResult {
-    rows: Vec<HashMap<String, serde_json::Value>>,
-    columns: Vec<QueryResultColumn>,
-}
-
-#[derive(Debug, Deserialize)]
-struct QueryResultColumn {
-    name: String,
-}
-
-struct MyTheme;
-
-impl Theme for MyTheme {
-    /// Formats an input prompt.
-    fn format_input_prompt(
-        &self,
-        f: &mut dyn fmt::Write,
-        prompt: &str,
-        default: Option<&str>,
-    ) -> fmt::Result {
-        match default {
-            Some(default) if prompt.is_empty() => write!(f, "[{}] ", default),
-            Some(default) => write!(f, "{} [{}] ", prompt, default),
-            None => write!(f, "{} ", prompt),
-        }
-    }
-
-    /// Formats an input prompt after selection.
-    #[inline]
-    fn format_input_prompt_selection(
-        &self,
-        f: &mut dyn fmt::Write,
-        prompt: &str,
-        sel: &str,
-    ) -> fmt::Result {
-        write!(f, "{} {}", prompt, sel)
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -98,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             db_name.to_string() + "-#"
         };
 
-        let input = Input::<String>::with_theme(&MyTheme)
+        let input = Input::<String>::with_theme(&CliTheme)
             .with_prompt(prompt)
             .history_with(&mut history)
             .allow_empty(true)
@@ -125,40 +54,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             cmd_lines.push(input);
         }
     }
-
-    Ok(())
-}
-
-async fn run_query(query: &str, conn: &Connection<'_>) -> Result<(), Box<dyn std::error::Error>> {
-    let QueryResult { rows, columns } = conn.execute(query).await?;
-
-    if columns.is_empty() {
-        return Ok(());
-    }
-
-    let mut table = pt::Table::new();
-    table.set_format(*pt::format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-
-    let column_names: Vec<&String> = columns.iter().map(|c| &c.name).collect();
-    table.set_titles(column_names.iter().into());
-
-    for row in &rows {
-        table.add_row(
-            column_names
-                .iter()
-                .map(|&c| match row.get(c).unwrap() {
-                    serde_json::Value::String(v) => v.clone(),
-                    serde_json::Value::Number(v) => v.to_string(),
-                    serde_json::Value::Bool(v) => v.to_string(),
-                    serde_json::Value::Null => String::from(""),
-                    v => panic!("Failed to parse json value {}", v),
-                })
-                .into(),
-        );
-    }
-
-    table.printstd();
-    println!("({} rows)", rows.len());
 
     Ok(())
 }
